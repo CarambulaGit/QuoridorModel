714f80f207e800e1815076debd507b5192a449bf
Carambula <antibog6@gmail.com>
2021-11-07 17:20:44 +0200

    Fix AStarQuoridor

--- Pathfinding/AStarQuoridor.cs
+++ Pathfinding/AStarQuoridor.cs
@@ -1,33 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Project.Classes.Field;
+using Debug = UnityEngine.Debug;
 
 namespace Project.Classes.Pathfinding {
     public static class AStarQuoridor {
-        private static List<Point> _path = new List<Point>();
         private static List<Point> _finishes = new List<Point>();
+        public static Stopwatch stopWatch = new Stopwatch();
+
 
         public static List<Point> FindPath(FieldSpace[,] field, Point start, Predicate<Point> winningCondition,
             Func<Point, Point, float> heuristicLength) {
-            _path.Clear();
-            _finishes = CalculateFinishes(field.GetLength(0), field.GetLength(1), winningCondition);
+            var shortestPath = new List<Point>();
+            if (winningCondition(start)) {
+                return shortestPath;
+            }
+
+            _finishes = CalculateFinishes(field.GetLength(0), field.GetLength(1), winningCondition)
+                .OrderBy(finish => Point.ManhattanLengthFloat(start, finish)).ToList();
             foreach (var finish in _finishes) {
+                stopWatch.Start();
                 var path = AStar<FieldSpace>.FindPath(field, start, finish, heuristicLength);
+                stopWatch.Stop();
+                Debug.Log(stopWatch.ElapsedMilliseconds);
                 if (path == null) {
                     continue;
                 }
 
                 if (path.Count == 1) {
-                    return path;
+                    shortestPath.Clear();
+                    shortestPath.AddRange(path);
+                    return shortestPath;
                 }
 
-                if (path.Count < _path.Count || _path.Count == 0) {
-                    _path.Clear();
-                    _path.AddRange(path);
+                if (path.Count < shortestPath.Count || shortestPath.Count == 0) {
+                    shortestPath.Clear();
+                    shortestPath.AddRange(path);
                 }
             }
 
-            return _path;
+            return shortestPath;
         }
 
         public static bool IsTherePaths(FieldSpace[,] field, List<Pawn> pawns,
@@ -54,7 +68,7 @@ namespace Project.Classes.Pathfinding {
         private static List<Point> CalculateFinishes(int fieldYLen, int fieldXLen, Predicate<Point> winningCondition) {
             _finishes.Clear();
             for (var y = 0; y < fieldYLen; y += 2) {
-                for (var x = 0; x < fieldXLen; x +=2) {
+                for (var x = 0; x < fieldXLen; x += 2) {
                     var curPoint = new Point(y, x);
                     if (winningCondition(curPoint)) {
                         _finishes.Add(curPoint);
