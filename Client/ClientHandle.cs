1991b862d8dbaedc6f6936c27a3692c5d75f3d57
Carambula <carambuletka@gmail.com>
2021-12-19 20:07:20 +0200

    add Client

---
+++ Client/ClientHandle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Project.Classes.Field;
+using Project.Classes.Player;
+using UnityEngine;
+using NetworkPlayer = Project.Classes.Player.NetworkPlayer;
+
+public class ClientHandle {
+    public static void Welcome(Packet _packet) {
+        string _msg = _packet.ReadString();
+        int _myId = _packet.ReadInt();
+
+        Debug.Log($"Message from server: {_msg}");
+        Client.instance.myId = _myId;
+        ClientSend.WelcomeReceived();
+    }
+
+    public static void StartGame(Packet _packet) {
+        int _firstPlayerId = _packet.ReadInt();
+        int _secondPlayerId = _packet.ReadInt();
+        if (Client.instance.myId == _firstPlayerId) {
+            Client.instance.GameManager.CreateNetworkPlayerVsPlayer(_firstPlayerId, _secondPlayerId, true);
+        }
+        else {
+            Client.instance.GameManager.CreateNetworkPlayerVsPlayer(_secondPlayerId, _firstPlayerId, false);
+        }
+    }
+
+    public static void MakeMove(Packet _packet) {
+        int _playerId = _packet.ReadInt();
+        var moveType = (Player.MoveType) _packet.ReadInt();
+        var pos = _packet.ReadPoint();
+        var game = Client.instance.GameManager.Game;
+        var player = (NetworkPlayer) game.FindPlayerWithNetworkId(_playerId);
+        switch (moveType) {
+            case Player.MoveType.PlacingWall:
+                var wallType = (Wall.Type) _packet.ReadInt();
+                player.SetWallByServer(new Wall(pos, wallType));
+                break;
+            case Player.MoveType.Moving:
+                player.MovePawnByServer(pos);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ Client/ClientSend.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Project.Classes.Field;
+using Project.Classes.Player;
+using UnityEngine;
+
+public class ClientSend {
+    private static void SendTCPData(Packet _packet) {
+        _packet.WriteLength();
+        Client.instance.tcp.SendData(_packet);
+    }
+
+    #region Packets
+
+    public static void WelcomeReceived() {
+        using Packet _packet = new Packet((int) ClientPackets.welcomeReceived);
+        _packet.Write(Client.instance.myId);
+        _packet.Write(""/*UIManager.instance.usernameField.text*/); // todo remove
+
+        SendTCPData(_packet);
+    }
+    public static void SendMove(Point position) {
+        using Packet _packet = new Packet((int) ClientPackets.sendMove);
+        _packet.Write(Client.instance.myId);
+        _packet.Write((int) Player.MoveType.Moving);
+        _packet.Write(position);
+
+        SendTCPData(_packet);
+    }
+    public static void SendMove(Wall wall) {
+        using Packet _packet = new Packet((int) ClientPackets.sendMove);
+        _packet.Write(Client.instance.myId);
+        _packet.Write((int) Player.MoveType.PlacingWall);
+        _packet.Write(wall.Pos);
+        _packet.Write((int) wall.WallType);
+
+        SendTCPData(_packet);
+    }
+
+    #endregion
+}
